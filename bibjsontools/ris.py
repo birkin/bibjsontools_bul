708818a788d94f907f96c68c0e626b51ba99516c
birkin <birkin.diana@gmail.com>
2016-04-11 10:38:24 -0400

    refactors identifier-determination.

--- bibjsontools/ris.py
+++ bibjsontools/ris.py
@@ -14,8 +14,60 @@ from bibjsontools import from_openurl
 
 class RISMaker( object ):
 
-    def foo(self):
-        return 'bar'
+    def __init__(self):
+        self.FIELD_MAP = {
+            'access date': 'Y2',
+            'accession number': 'AN',
+            'alternate title': 'J2',
+            'author': 'AU',
+            'call number': 'CN',
+            'caption': 'CA',
+            'custom 3': 'C3',
+            'custom 4': 'C4',
+            'custom 5': 'C5',
+            'custom 7': 'C7',
+            'custom 8': 'C8',
+            'database provider': 'DP',
+            'date': 'DA',
+            'doi': 'DO',
+            'epub date': 'ET',
+            'figure': 'L4',
+            'file attachments': 'L1',
+            'institution': 'AD',
+            'issn': 'SN',
+            'issue': 'IS',
+            'journal': 'JF',
+            'keyword': 'KW',
+            'label': 'LB',
+            'language': 'LA',
+            'name of database': 'DB',
+            'nihmsid': 'C6',
+            'note': 'AB',
+            'notes': 'N1',
+            'number': 'IS',
+            'number of volumes': 'NV',
+            'original publication': 'OP',
+            'pages': 'SP',
+            'place published': 'CY',
+            'pmcid': 'C2',
+            'publisher': 'PB',
+            'reprint edition': 'RP',
+            'reviewed item': 'RI',
+            'secondary title': 'T2',
+            'section': 'SE',
+            'short title': 'ST',
+            'start page': 'M2',
+            'subsidiary author': 'A4',
+            'tertiary author': 'A3',
+            'tertiary title': 'T3',
+            'title': 'TI',
+            'translated author': 'TA',
+            'translated title': 'TT',
+            'type ': 'TY',
+            'url': 'UR',
+            'volume': 'VL',
+            'year': 'PY'
+            }
 
     def convert_to_ris( self, bib_dct ):
         """ Converts bibjson data to ris data. """
@@ -24,20 +76,10 @@ class RISMaker( object ):
         for k,v in bib_dct.items():
             if k == 'author':
                 ris_dct = self._check_author( ris_dct, v )
-
             elif k == 'journal':
                 ris_dct['JF'] = v.get('name')
             elif k == 'identifier':
-                for idt in v:
-                    this = idt['id']
-                    if idt['type'] == 'doi':
-                        ris_dct['DO'] = this
-                    elif idt['type'] == 'issn':
-                        ris_dct['SN'] = this
-                    elif idt['type'] == 'isbn':
-                        ris_dct['SN'] = this
-                    #elif idt['type'] == 'pmid':
-                    #   ris_dct[]
+                ris_dct = self._check_identifier( ris_dct, v )
             else:
                 ris_k = FIELD_MAP.get(k, None)
                 if ris_k:
@@ -57,7 +99,7 @@ class RISMaker( object ):
         return ris_type
 
     def _check_author( self, ris_dct, author_list ):
-        """ Checks author value (a list).
+        """ Checks author value (a list of dcts).
             Called by convert_to_ris() """
         for author in author_list:
             name = author.get( 'name' )
@@ -66,16 +108,26 @@ class RISMaker( object ):
                 break
         return ris_dct
 
+    def _check_identifier( self, ris_dct, identifier_list ):
+        """ Checks identifier value (a list of dcts).
+            Called by convert_to_ris() """
+        for identifier_dct in identifier_list:
+            id_value = identifier_dct['id']
+            if identifier_dct['type'] == 'doi':
+                ris_dct['DO'] = id_value
+            elif identifier_dct['type'] == 'issn':
+                ris_dct['SN'] = id_value
+            elif identifier_dct['type'] == 'isbn':
+                ris_dct['SN'] = id_value
+        return ris_dct
+
     # def convert_to_ris( self, bib_dct ):
     #     """ Converts bibjson data to ris data. """
     #     ris_dct = {}
     #     ris_dct['TY'] = self._handle_type( bib_dct['type'] )
     #     for k,v in bib_dct.items():
     #         if k == 'author':
-    #             for author in v:
-    #                 name = author.get('name')
-    #                 if name:
-    #                     ris_dct['AU'] = name
+    #             ris_dct = self._check_author( ris_dct, v )
     #         elif k == 'journal':
     #             ris_dct['JF'] = v.get('name')
     #         elif k == 'identifier':
