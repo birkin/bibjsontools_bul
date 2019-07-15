04f34e37a654ca79e1d12c7495b91683ec013f83
birkin <birkin.diana@gmail.com>
2019-07-15 13:56:36 -0400

    updates version.

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
 import json
 from setuptools import find_packages, setup
 
 
 setup(
     name='bibjsontools',
-    version='0.4e',
+    version='0.5a',
     packages=find_packages(),
     install_requires=[],
 )
